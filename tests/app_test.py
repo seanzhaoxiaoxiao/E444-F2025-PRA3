f1d7a78a959548250d54e71eac5e00f92e8938bb
Sean Zhao Xiaoxiao <sean.zhaoxiaoxiao@hotmail.com>
2025-09-29 00:46:41 -0400

    Adapt Login Required

--- tests/app_test.py
+++ tests/app_test.py
@@ -90,7 +90,11 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
 
@@ -121,3 +125,12 @@ def test_search_route(client):
     assert rv.status_code == 200
     assert b"TestTitle" not in rv.data
     assert b"TestText" not in rv.data
+
+def test_login_required_for_delete(client):
+    """Ensure that delete route requires login"""
+    # Try to delete without logging in
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert rv.status_code == 401
+    assert data["status"] == 0
+    assert data["message"] == "Please log in."
